8a51a39fabbba16be6e5b346a1de89852ab3b50f
hsj._.06 <gtw030488@gmail.com>
2022-10-25 15:05:45 +0900

    👨‍🎓 :: Queue

--- c언어/c언어/main.c
+++ c언어/c언어/main.c
@@ -855,52 +855,132 @@
 //    printf("이름 : %s\n", oe.name);
 //    printf("주소 : %s\n", oe.address);
 //}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <stdbool.h>
+//
+//#define MAX_STACK_SIZE 100
+//
+//typedef struct {
+//    int data[MAX_STACK_SIZE];
+//    int top;
+//} StackType;
+//StackType stack[MAX_STACK_SIZE];
+//
+//bool is_empty(StackType *s) {
+//    return (s -> top == -1);
+//}
+//bool is_full(StackType *s) {
+//    return (s -> top == MAX_STACK_SIZE - 1);
+//}
+//void push(StackType item) {
+//    if (is_full()) {
+//        printf("스택 포화 에러\n");
+//        return;
+//    } else stack[++top] = item;
+//}
+//StackType pop(void) {
+//    if (is_empty()) {
+//        printf("스택 공백 에러\n");
+//        exit(1);
+//    } else return stack[top--];
+//}
+//StackType peek(void) {
+//    if (is_empty()) {
+//        printf("스택 공백 에러\n");
+//        exit(1);
+//    } else return stack[top];
+//}
+//void init_stack(StackType *s) {
+//    s -> top = -1;
+//}
+//
+//int eval(char exp[]) {
+//    int op1, op2, value, i = 0;
+//    unsigned long len = strlen(exp);
+//    char ch;
+//    StackType s;
+//
+//
+//}
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
 
-#define MAX_STACK_SIZE 100
+#define MAX_QUEUE_SIZE 5
+typedef struct queue { // 큐 타입
+   int  data[MAX_QUEUE_SIZE];
+   int  front, rear;
+} QueueType;
 
-typedef struct {
-    int data[MAX_STACK_SIZE];
-    int top;
-} StackType;
-StackType stack[MAX_STACK_SIZE];
-
-bool is_empty(StackType *s) {
-    return (s -> top == -1);
-}
-bool is_full(StackType *s) {
-    return (s -> top == MAX_STACK_SIZE - 1);
-}
-void push(StackType item) {
-    if (is_full()) {
-        printf("스택 포화 에러\n");
-        return;
-    } else stack[++top] = item;
-}
-StackType pop(void) {
-    if (is_empty()) {
-        printf("스택 공백 에러\n");
-        exit(1);
-    } else return stack[top--];
-}
-StackType peek(void) {
-    if (is_empty()) {
-        printf("스택 공백 에러\n");
-        exit(1);
-    } else return stack[top];
-}
-void init_stack(StackType *s) {
-    s -> top = -1;
+// 초기화 함수
+void init_queue(QueueType* q)
+{
+   q->front = q->rear = 0;
 }
 
-int eval(char exp[]) {
-    int op1, op2, value, i = 0;
-    unsigned long len = strlen(exp);
-    char ch;
-    StackType s;
-    
-    
+// 공백 상태 검출 함수
+int is_empty(QueueType* q)
+{
+   if (q->rear == q->front)
+      return 1;
+   else
+      return 0;
+}
+
+// 포화 상태 검출 함수
+int is_full(QueueType* q)
+{
+   if ((q->rear + 1) % MAX_QUEUE_SIZE == q->front)
+      return 1;
+   else
+      return 0;
+}
+
+
+// 삽입 함수
+void enqueue(QueueType* q, int item)
+{
+   if (is_full(q))
+      printf("큐가 포화상태입니다");
+   q->data[++(q->rear) % MAX_QUEUE_SIZE] = item;
+}
+
+// 삭제 함수
+int dequeue(QueueType* q)
+{
+   if (is_empty(q))
+      printf("큐가 공백상태입니다");
+   return q->data[(++q->front) % MAX_QUEUE_SIZE];
+}
+// 원형큐 출력 함수
+void queue_print(QueueType* q)
+{
+   for (int i = q->front; i != q->rear; ) {
+      i = (i + 1) % (MAX_QUEUE_SIZE);
+      printf("%d ", q->data[i]);
+   }
+   printf("\n");
+}
+int main(void)
+{
+   QueueType q;
+   int num;
+
+   init_queue(&q);
+
+   enqueue(&q, 10);
+   queue_print(&q);
+   enqueue(&q, 20);
+   queue_print(&q);
+   enqueue(&q, 30);
+   queue_print(&q);
+
+   dequeue(&q);
+   queue_print(&q);
+   dequeue(&q);
+   queue_print(&q);
+
+   return 0;
 }
