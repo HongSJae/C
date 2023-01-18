5b9cd0d4738606c856e34d342c5ea289a2a0068e
hsj._.06 <gtw030488@gmail.com>
2023-01-18 14:46:08 +0900

    백준 2739번

--- c언어/c언어/Baekjoon.c
+++ c언어/c언어/Baekjoon.c
@@ -80,17 +80,27 @@
 //    }
 //    printf("%d", fac);
 //}
-//MARK: - 10872번
+//MARK: - 10950번
+//#include <stdio.h>
+//
+//int main() {
+//    int count = 0;
+//    scanf("%d", &count);
+//    int a[100], b[100] = {0};
+//    for (int i = 0; i < count; i++) {
+//        scanf("%d %d", &a[i], &b[i]);
+//    }
+//    for (int i = 0; i < count; i++) {
+//        printf("%d\n", a[i] + b[i]);
+//    }
+//}
+//MARK: - 2739번
 #include <stdio.h>
 
 int main() {
     int count = 0;
     scanf("%d", &count);
-    int a[100], b[100] = {0};
-    for (int i = 0; i < count; i++) {
-        scanf("%d %d", &a[i], &b[i]);
-    }
-    for (int i = 0; i < count; i++) {
-        printf("%d\n", a[i] + b[i]);
+    for (int i = 1; i <= 9; i++) {
+        printf("%d * %d = %d\n", count, i, count * i);
     }
 }
