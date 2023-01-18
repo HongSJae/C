d263c95333c3cb6eef5aaf18366db34a3b08bf97
hsj._.06 <gtw030488@gmail.com>
2023-01-18 21:38:31 +0900

    백준 2744번

--- c언어/c언어/Baekjoon.c
+++ c언어/c언어/Baekjoon.c
@@ -220,11 +220,34 @@
 //    printf("%d", c);
 //}
 //MARK: - 2743번
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main() {
+//    char s[100] = {""};
+//    scanf("%s", s);
+//    printf("%ld", strlen(s));
+//}
+//MARK: - 2744번
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    char s[100] = {""};
-    scanf("%s", s);
-    printf("%ld", strlen(s));
+int main()
+{
+    //97~122 a~z
+    //65~90 A~Z
+    char str[100] = " ";
+    int length, i;
+
+    scanf("%s", str);
+
+    length = strlen(str);
+
+    for (i = 0; i < length; i++) {
+        if (str[i] >= 97 && str[i] <= 122)
+            str[i] -= 32;
+        else if (str[i] >= 65 && str[i] <= 90)
+            str[i] += 32;
+    }
+    printf("%s", str);
 }
