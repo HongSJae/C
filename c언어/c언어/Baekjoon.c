4f64046e3f79f6ecf63fabc5a09477d5604ca784
hsj._.06 <gtw030488@gmail.com>
2023-01-18 21:25:36 +0900

    백준  11654번

--- c언어/c언어/Baekjoon.c
+++ c언어/c언어/Baekjoon.c
@@ -188,26 +188,34 @@
 //            printf("%d \n", i + 1);
 //}
 //MARK: - 2738번
+//#include <stdio.h>
+//
+//int main() {
+//    int n, m;
+//    scanf("%d %d", &n, &m);
+//    int a[100][100], b[100][100];
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < m; j++) {
+//            scanf("%d", &a[i][j]);
+//        }
+//    }
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < m; j++) {
+//            scanf("%d", &b[i][j]);
+//        }
+//    }
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < m; j++) {
+//            printf("%d ", a[i][j] + b[i][j]);
+//        }
+//        printf("\n");
+//    }
+//}
+//MARK: - 11654번
 #include <stdio.h>
 
 int main() {
-    int n, m;
-    scanf("%d %d", &n, &m);
-    int a[100][100], b[100][100];
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            scanf("%d", &a[i][j]);
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            scanf("%d", &b[i][j]);
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            printf("%d ", a[i][j] + b[i][j]);
-        }
-        printf("\n");
-    }
+    char c;
+    scanf("%c", &c);
+    printf("%d", c);
 }
