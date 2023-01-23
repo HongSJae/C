43a112e0140eae79ec67c13277100a8523befcc3
hsj._.06 <gtw030488@gmail.com>
2023-01-24 01:40:50 +0900

    백준 2742번

--- c언어/c언어/Baekjoon.c
+++ c언어/c언어/Baekjoon.c
@@ -561,18 +561,30 @@
 //        printf("\n");
 //    }
 //}
-//MARK: -  2577번
+//MARK: - 2577번
+//#include <stdio.h>
+//
+//int arr[10];
+//
+//int main(){
+//    int A,B,C;
+//    int num;
+//    scanf("%d %d %d",&A,&B,&C);
+//    num = A * B * C;
+//    for(int i=num;i>0;i/=10)
+//        arr[i%10]++;
+//    for(int i=0;i<10;i++)
+//        printf("%d\n",arr[i]);
+//}
+//MARK: - 2742번
 #include <stdio.h>
 
 int arr[10];
 
 int main(){
-    int A,B,C;
     int num;
-    scanf("%d %d %d",&A,&B,&C);
-    num = A * B * C;
-    for(int i=num;i>0;i/=10)
-        arr[i%10]++;
-    for(int i=0;i<10;i++)
-        printf("%d\n",arr[i]);
+    scanf("%d", &num);
+    for (int i = num; i > 0; i--) {
+        printf("%d\n", i);
+    }
 }
